c624f8ed445bff3543a0afbcaa449b01c5e93559
Ashutosh <ashutoshtips@gmail.com>
2024-06-28 00:23:49 +0530

    Update Configure Automatic Driver Update.bat
    - Fixes for 24H2.
    - Added More Tweaks.

--- Configure Automatic Driver Update.bat
+++ Configure Automatic Driver Update.bat
@@ -45,10 +45,11 @@ goto begin
 
 :op1
 :: ----------------------------------------------------------
-:: -------Disable Windows Update device driver search--------
+:: -Disable device metadata retrieval (breaks auto updates)--
 :: ----------------------------------------------------------
-echo --- Disable Windows Update device driver search
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" /v "SearchOrderConfig" /t REG_DWORD /d 0 /f
+echo --- Disable device metadata retrieval (breaks auto updates)
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Device Metadata" /v "PreventDeviceMetadataFromNetwork" /t REG_DWORD /d 1 /f
+PowerShell -ExecutionPolicy Unrestricted -Command "reg add 'HKLM\SOFTWARE\Policies\Microsoft\Windows\Device Metadata' /v 'PreventDeviceMetadataFromNetwork' /t 'REG_DWORD' /d '1' /f"
 :: ----------------------------------------------------------
 
 
@@ -56,7 +57,15 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" /v "Sea
 :: ----Disable inclusion of drivers with Windows updates-----
 :: ----------------------------------------------------------
 echo --- Disable inclusion of drivers with Windows updates
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ExcludeWUDriversInQualityUpdate" /t REG_DWORD /d 1 /f
+PowerShell -ExecutionPolicy Unrestricted -Command "reg add 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate' /v 'ExcludeWUDriversInQualityUpdate' /t 'REG_DWORD' /d '1' /f"
+:: ----------------------------------------------------------
+
+
+:: ----------------------------------------------------------
+:: -------Disable Windows Update device driver search--------
+:: ----------------------------------------------------------
+echo --- Disable Windows Update device driver search
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" /v "SearchOrderConfig" /t "REG_DWORD" /d "1" /f
 :: ----------------------------------------------------------
 
 
@@ -69,19 +78,26 @@ exit /b 0
 
 :op2
 echo Enabling drivers download via Windows update...
-:: Disable inclusion of drivers with Windows updates (revert)
-echo --- Disable inclusion of drivers with Windows updates (revert)
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ExcludeWUDriversInQualityUpdate" /t REG_DWORD /d 0 /f
+:: Disable device metadata retrieval (breaks auto updates)
+echo --- Enable device metadata retrieval
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Device Metadata" /v "PreventDeviceMetadataFromNetwork" /t REG_DWORD /d 0 /f
+PowerShell -ExecutionPolicy Unrestricted -Command "reg delete 'HKLM\SOFTWARE\Policies\Microsoft\Windows\Device Metadata' /v 'PreventDeviceMetadataFromNetwork' /f 2>$null"
+:: ----------------------------------------------------------
+
+
+:: Enable inclusion of drivers with Windows updates
+echo --- Enable inclusion of drivers with Windows updates 
+PowerShell -ExecutionPolicy Unrestricted -Command "reg delete 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate' /v 'ExcludeWUDriversInQualityUpdate' /f 2>$null"
 :: ----------------------------------------------------------
 
 
 :: ----------------------------------------------------------
-:: ---Disable Windows Update device driver search (revert)---
+:: ---Enable Windows Update device driver search ---
 :: ----------------------------------------------------------
-echo --- Disable Windows Update device driver search (revert)
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" /v "SearchOrderConfig" /t REG_DWORD /d 1 /f
+echo --- Enable Windows Update device driver search 
+:: Key exists with value "4" (All data) since Windows 10 22H2, Windows 11 22H3
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" /v "SearchOrderConfig" /t "REG_DWORD" /d "1" /f
 :: ----------------------------------------------------------
-
 
 :: Pause the script to view the final state
 pause
