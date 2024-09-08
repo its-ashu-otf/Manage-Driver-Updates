1724a7bad93fd506922647bbcdc2e65cf927a6b7
Ashutosh <ashutoshtips@gmail.com>
2024-09-08 16:17:30 +0530

    Update Configure Automatic Driver Update.bat
    - Many Core Fixes.
    - More Error Handling and Forcing Driver Update to stop
    - Many more Changes too...

--- Configure Automatic Driver Update.bat
+++ Configure Automatic Driver Update.bat
@@ -1,6 +1,6 @@
 @echo off
 color 0a
-title Configure Driver Update Through Windows Update v3.1
+title Configure Driver Update Through Windows Update v4.0
 :: Ensure admin privileges
 fltmc >nul 2>&1 || (
     echo Administrator privileges are required.
@@ -22,17 +22,17 @@ setlocal EnableExtensions DisableDelayedExpansion
 ::: ____) |   | |   | |__| || |      | |__| || | \ \  _| |_    \  /   | |____ | | \ \  | |__| || |     | |__| | / ____ \    | |   | |____ 
 :::|_____/    |_|    \____/ |_|      |_____/ |_|  \_\|_____|    \/    |______||_|  \_\  \____/ |_|     |_____/ /_/    \_\   |_|   |______|                                                                                                                                       
 :::
-:::				___  _   _     _ ___ ____     ____ ____ _  _ _  _     ____ ___ ____ 
-:::				|__]  \_/  .   |  |  [__      |__| [__  |__| |  |     |  |  |  |___ 
-:::				|__]   |   .   |  |  ___] ___ |  | ___] |  | |__| ___ |__|  |  |    
+:::                             ___  _   _     _ ___ ____     ____ ____ _  _ _  _     ____ ___ ____ 
+:::			            	    |__]  \_/  .   |  |  [__      |__| [__  |__| |  |     |  |  |  |___ 
+:::				                |__]   |   .   |  |  ___] ___ |  | ___] |  | |__| ___ |__|  |  |    
 :::                                                     								
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 :begin
 echo       Configure Windows Driver Update
 echo =============================================
 echo.
-echo 1) Disable Driver Update Offered By Windows
-echo 2) Enable Driver Update Offered By Windows
+echo 1) Disable Driver Updates Offered By Windows (Recommended)
+echo 2) Enable Driver Updates Offered By Windows
 echo =============================================
 echo.
 echo,
@@ -44,14 +44,6 @@ echo Please Pick an option:
 goto begin
 
 :op1
-:: ----------------------------------------------------------
-:: -Disable device metadata retrieval (breaks auto updates)--
-:: ----------------------------------------------------------
-echo --- Disable device metadata retrieval (breaks auto updates)
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Device Metadata" /v "PreventDeviceMetadataFromNetwork" /t REG_DWORD /d 1 /f
-PowerShell -ExecutionPolicy Unrestricted -Command "reg add 'HKLM\SOFTWARE\Policies\Microsoft\Windows\Device Metadata' /v 'PreventDeviceMetadataFromNetwork' /t 'REG_DWORD' /d '1' /f"
-:: ----------------------------------------------------------
-
 
 :: ----------------------------------------------------------
 :: ----Disable inclusion of drivers with Windows updates-----
@@ -68,6 +60,24 @@ echo --- Disable Windows Update device driver search
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" /v "SearchOrderConfig" /t "REG_DWORD" /d "1" /f
 :: ----------------------------------------------------------
 
+powershell.exe -Command "& {
+    if (!(Test-Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Device Metadata')) {
+        New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Device Metadata' -Force | Out-Null
+    }
+    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Device Metadata' -Name 'PreventDeviceMetadataFromNetwork' -Type DWord -Value 1
+
+    if (!(Test-Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DriverSearching')) {
+        New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DriverSearching' -Force | Out-Null
+    }
+    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DriverSearching' -Name 'DontPromptForWindowsUpdate' -Type DWord -Value 1
+    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DriverSearching' -Name 'DontSearchWindowsUpdate' -Type DWord -Value 1
+    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DriverSearching' -Name 'DriverUpdateWizardWuSearchEnabled' -Type DWord -Value 0
+
+    if (!(Test-Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate')) {
+        New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate' | Out-Null
+    }
+    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate' -Name 'ExcludeWUDriversInQualityUpdate' -Type DWord -Value 1
+}"
 
 :: Pause the script to view the final state
 pause
@@ -77,13 +87,6 @@ endlocal
 exit /b 0
 
 :op2
-echo Enabling drivers download via Windows update...
-:: Disable device metadata retrieval (breaks auto updates)
-echo --- Enable device metadata retrieval
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Device Metadata" /v "PreventDeviceMetadataFromNetwork" /t REG_DWORD /d 0 /f
-PowerShell -ExecutionPolicy Unrestricted -Command "reg delete 'HKLM\SOFTWARE\Policies\Microsoft\Windows\Device Metadata' /v 'PreventDeviceMetadataFromNetwork' /f 2>$null"
-:: ----------------------------------------------------------
-
 
 :: Enable inclusion of drivers with Windows updates
 echo --- Enable inclusion of drivers with Windows updates 
@@ -98,6 +101,25 @@ echo --- Enable Windows Update device driver search
 :: Key exists with value "4" (All data) since Windows 10 22H2, Windows 11 22H3
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" /v "SearchOrderConfig" /t "REG_DWORD" /d "1" /f
 :: ----------------------------------------------------------
+
+powershell.exe -Command "& {
+    Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Device Metadata' -Name 'PreventDeviceMetadataFromNetwork' -ErrorAction SilentlyContinue
+    if (!(Test-Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Device Metadata')) {
+        Remove-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Device Metadata' -ErrorAction SilentlyContinue
+    }
+
+    Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DriverSearching' -Name 'DontPromptForWindowsUpdate' -ErrorAction SilentlyContinue
+    Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DriverSearching' -Name 'DontSearchWindowsUpdate' -ErrorAction SilentlyContinue
+    Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DriverSearching' -Name 'DriverUpdateWizardWuSearchEnabled' -ErrorAction SilentlyContinue
+    if (!(Test-Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DriverSearching')) {
+        Remove-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DriverSearching' -ErrorAction SilentlyContinue
+    }
+
+    Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate' -Name 'ExcludeWUDriversInQualityUpdate' -ErrorAction SilentlyContinue
+    if (!(Test-Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate')) {
+        Remove-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate' -ErrorAction SilentlyContinue
+    }
+}"
 
 :: Pause the script to view the final state
 pause
